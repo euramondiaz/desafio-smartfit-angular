fa500faf5aecb36055773f7a8fec27f2d6a333d9
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-18 11:23:47 -0300

    create, style and structure forms component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormsComponent } from './components/forms/forms.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FormsComponent,
 
 
   ],
