e0807946567530941e06b0ffa75e17d011258a50
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-18 14:50:35 -0300

    first add form control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 
 @NgModule({
@@ -10,11 +14,11 @@ import { FormsComponent } from './components/forms/forms.component';
     AppComponent,
     HeaderComponent,
     FormsComponent,
-
-
   ],
+
+
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup, } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.css']
+  styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
+export class FormsComponent  {
   results= [];
+  formGroup!: FormGroup;
 
+  constructor (private formBuilder: FormBuilder) {}
+
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed:  false
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.formGroup.value)
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
 }
+
